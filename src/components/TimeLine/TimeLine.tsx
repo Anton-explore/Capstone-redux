a223093fcf81a3073aa4f0e817d041cbee3bd7de
Igor Ieromenko <Igor.Ieromenko@3shape.com>
2023-03-21 17:03:42 +0000

    add useTimeLineHook

--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -1,11 +1,7 @@
-import { forwardRef, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-// import { useDispatch } from 'react-redux';
-import { getEducation } from '../../features/education/educationSlice';
-import { RootState, useAppDispatch } from '../../store/store';
+import { forwardRef } from 'react';
+import useTimeLineHook from '../../hooks/useTimeLineHook';
 import Heading from '../Box/Heading';
 import { StyledInfo, StyledTitle, StyledText, StyledItem, StyledInfoBlock, StyledDateBlock } from './TimeLine.styles';
-
 
 export interface TimelineProps {
     id: number,
@@ -26,26 +22,16 @@ export type Ref = HTMLElement;
 
 const TimeLine = forwardRef<Ref, IdProps>(({ id }: IdProps, forwardedRef ) => {
 
-
-    // getEducation();
-    const dispatch = useAppDispatch();
-    const educationsList = useSelector((store: RootState) => store.education.educations);
-    const status = useSelector((store: RootState) => store.education.isLoading);
-    const error = useSelector((store: RootState) => store.education.error);
-
-    useEffect(() => {
-        dispatch(getEducation());
-    }, [dispatch])
+    const { educationsList, error, status} = useTimeLineHook();
 
     return (
         <section ref={forwardedRef} id={id}>
             <Heading title='Education' />
-            
             {status && <p>Loading...</p>}
             {error !== null && (
                 <p>Oops, some error occured... {error}</p>
             )}
-            {educationsList.length > 0 && (
+            {!!educationsList.length && (
                 <StyledInfo className="timeline-list">
                     {educationsList.map((item: TimelineProps) =>
                         <StyledItem key={item.id}>
